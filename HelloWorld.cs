ccd4ee1397f50b7e438b35f5d4a04cf79f71c9d9
DavidMkhi <david.mkhitar09@gmail.com>
2024-05-10 19:04:11 +0300

    HelloWorld

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // prindib consooli
+        print(2024);
+        print("Hello My World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
